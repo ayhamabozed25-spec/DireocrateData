f9e58d51b396389c9aa1e01dda1ae858f4a38a10
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-27 03:35:29 +0300

    Update App.js

--- direcoratedata/src/App.js
+++ direcoratedata/src/App.js
@@ -1,6 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DepartmentsForm from "./components/DepartmentsForm";
+import DivisionsForm from "./components/DivisionsForm";
 import EmployeesForm from "./components/EmployeesForm";
+import DevicesForm from "./components/DevicesForm";
+import CarsForm from "./components/CarsForm";
+import FurnitureForm from "./components/FurnitureForm";
+import ProjectsForm from "./components/ProjectsForm";
+import ProjectChallengesForm from "./components/ProjectChallengesForm";
+import ServicesForm from "./components/ServicesForm";
+import StrengthsWeaknessesForm from "./components/StrengthsWeaknessesForm";
 import Search from "./components/Search";
 
 function App() {
@@ -8,7 +16,15 @@ function App() {
     <div className="container mt-4">
       <h1 className="mb-4">وثيقة الخطة التنفيذية</h1>
       <DepartmentsForm />
+      <DivisionsForm />
       <EmployeesForm />
+      <DevicesForm />
+      <CarsForm />
+      <FurnitureForm />
+      <ProjectsForm />
+      <ProjectChallengesForm />
+      <ServicesForm />
+      <StrengthsWeaknessesForm />
       <Search />
     </div>
   );
