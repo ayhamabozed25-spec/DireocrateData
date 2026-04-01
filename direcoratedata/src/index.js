06f684caddf7b5a22e6c8eb6f10568bea23a204a
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-04-01 13:16:14 +0300

    Update index.js

--- direcoratedata/src/index.js
+++ direcoratedata/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./components/AuthContext";
 
 root.render(
   <AuthProvider>
